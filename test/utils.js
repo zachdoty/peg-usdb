997c7553f41799a224d12f9b00f6f46ae95b5b2b
smartlaw-palis <cpalis@teleport.earth>
2019-07-31 06:42:39 +0800

    added PegSettings tests

--- test/utils.js
+++ test/utils.js
@@ -20,6 +20,8 @@ const ensureException = error => {
 }
 
 const createContracts = async accounts => {
+    const signers =  accounts.slice(4, 6);
+
     const RegistryContract = await ContractRegistry.new();
 
     const ContractIdsContract = await ContractIds.new();
@@ -33,17 +35,13 @@ const createContracts = async accounts => {
     const StableTokenContract = await StableToken.new("Bancor USD", "BUSD", 18, InstanceRegistryContract.address);
     const VaultAContract = await Vault.new(InstanceRegistryContract.address);
     const VaultBContract = await Vault.new(InstanceRegistryContract.address);
-    const PegSettingsContract = await PegSettings.new(InstanceRegistryContract.address);
     const PegLogicContract = await PegLogic.new(InstanceRegistryContract.address);
     const LogicActionsContract = await LogicActions.new(InstanceRegistryContract.address);
     const AuctionActionsContract = await AuctionActions.new(InstanceRegistryContract.address);
     const OracleContract = await Oracle.new();
 
-    await PegSettingsContract.authorize(PegLogicContract.address, true);
-    await PegSettingsContract.authorize(LogicActionsContract.address, true);
-    await PegSettingsContract.authorize(AuctionActionsContract.address, true);
-    await PegSettingsContract.authorize(VaultAContract.address, true);
-    await PegSettingsContract.authorize(VaultBContract.address, true);
+    const defaultAddresses = [PegLogicContract.address, LogicActionsContract.address, AuctionActionsContract.address, VaultAContract.address, VaultBContract.address];
+    const PegSettingsContract = await PegSettings.new(signers, defaultAddresses);
 
     await InstanceRegistryContract.registerAddress(await ContractIdsContract.COLLATERAL_TOKEN.call(), CollateralTokenContract.address);
     await InstanceRegistryContract.registerAddress(await ContractIdsContract.PEGUSD_TOKEN.call(), PEGUSDTokenContract.address);
@@ -56,13 +54,14 @@ const createContracts = async accounts => {
     await InstanceRegistryContract.registerAddress(await ContractIdsContract.PEG_LOGIC_ACTIONS.call(), LogicActionsContract.address);
     await InstanceRegistryContract.registerAddress(await ContractIdsContract.AUCTION_ACTIONS.call(), AuctionActionsContract.address);
 
-
     // RELAY TOKEN
     const RelayTokenContract = await StableToken.new("PEGUSD:BUSD Relay Token", "PEGUSD:BUSD", 18, InstanceRegistryContract.address);
     const ConverterContract = await BancorConverter.new(RelayTokenContract.address);
     await InstanceRegistryContract.registerAddress(await ContractIdsContract.FEE_RECIPIENT.call(), ConverterContract.address);
     
     return {
+        Signers: signers,
+        DefaultAuthorization: defaultAddresses,
         RegistryContract: RegistryContract,
         ContractIdsContract: ContractIdsContract,
         CollateralTokenContract: CollateralTokenContract,
